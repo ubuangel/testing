0b044f00d1a5a336b51bf01e082ac811be48857f
angel <andresbjar97@gmail.com>
2021-12-23 08:30:45 -0500

    para pruebas

---
+++ test_utils.py
@@ -0,0 +1,20 @@
+import unittest
+
+import utils
+
+
+class TestUtils(unittest.TestCase):
+    def test_esprimo(self):
+        self.assertFalse(utils.esprimo(4))
+        self.assertTrue(utils.esprimo(2))
+        self.assertTrue(utils.esprimo(3))
+        self.assertFalse(utils.esprimo(8))
+        self.assertFalse(utils.esprimo(10))
+        self.assertTrue(utils.esprimo(7))
+        self.assertEqual(utils.esprimo(-3),
+                         "numeros negativos no son permitidos")
+
+
+if __name__ == '__main__':
+    unittest.main()
+te
---
+++ utils.py
@@ -0,0 +1,22 @@
+import math
+def esprimo(n):
+    if n<0:
+        return "numeros negativos no son permitidos"
+    if n<=1:
+        return False
+
+    if n==2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    for i in range(2,int( math.sqrt(n))+1):
+        if n%i==0:
+            return False
+
+
+    return True
+def cubico(n):
+    return n*n*n
+def di_hola(cadena):
+    return "hola"+cadena
